bfa38d77520142ea4a3628c8f38862695ea1f905
MoxaSheth <shethmoxa01@gmail.com>
2026-03-13 22:44:45 +0530

    commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",       // your MySQL username
-  password: "your_password",       // your MySQL password
+  password: "",       // your MySQL password
   database: "eventspark"
 });
 
